5db7c514d4d20f22b5430ca963f5be203d36e33a
Baibhav Kumar <baibhav.kumar.122003@gmail.com>
2023-08-15 14:26:43 +0530

    .

--- src/Components/Header/FlipkartHeader.js
+++ src/Components/Header/FlipkartHeader.js
@@ -15,46 +15,46 @@ const FlipkartHeader = () => {
       <div className="logo-container">
         <Link to="/">Flipkart</Link>
       </div>
-      <div className="account-container">
-        <button className="account-button" onClick={toggleDropdown}>
-          My Account
-        </button>
-        {isDropdownOpen && (
-          <div className="dropdown-menu">
-            {(localStorage.getItem("userEmail") ||
-              localStorage.getItem("sellerEmail")) && (
-                <Link to="/user/rewards">
-                  <button className="dropdown-item">Loyalty Points</button>{" "}
-                </Link>
-              )}
-            <Link to="/login">
-              <button
-                className="dropdown-item"
-                onClick={() => {
-                  if (
-                    localStorage.getItem("userEmail") &&
-                    localStorage.getItem("userName")
-                  ) {
-                    localStorage.removeItem("userEmail");
-                    localStorage.removeItem("userName");
-                  }
+      {(localStorage.getItem("userEmail") ||
+        localStorage.getItem("sellerEmail")) && (
+        <div className="account-container">
+          <button className="account-button" onClick={toggleDropdown}>
+            My Account
+          </button>
+          {isDropdownOpen && (
+            <div className="dropdown-menu">
+              <Link to="/user/rewards">
+                <button className="dropdown-item">Loyalty Points</button>{" "}
+              </Link>
+              <Link to="/login">
+                <button
+                  className="dropdown-item"
+                  onClick={() => {
+                    if (
+                      localStorage.getItem("userEmail") &&
+                      localStorage.getItem("userName")
+                    ) {
+                      localStorage.removeItem("userEmail");
+                      localStorage.removeItem("userName");
+                    }
 
-                  if (
-                    localStorage.getItem("sellerEmail") &&
-                    localStorage.getItem("sellerName")
-                  ) {
-                    localStorage.removeItem("sellerEmail");
-                    localStorage.removeItem("sellerName");
-                  }
-                  // approveService(1000);
-                }}
-              >
-                Logout
-              </button>
-            </Link>
-          </div>
-        )}
-      </div>
+                    if (
+                      localStorage.getItem("sellerEmail") &&
+                      localStorage.getItem("sellerName")
+                    ) {
+                      localStorage.removeItem("sellerEmail");
+                      localStorage.removeItem("sellerName");
+                    }
+                    // approveService(1000);
+                  }}
+                >
+                  Logout
+                </button>
+              </Link>
+            </div>
+          )}
+        </div>
+      )}
     </header>
   );
 };
