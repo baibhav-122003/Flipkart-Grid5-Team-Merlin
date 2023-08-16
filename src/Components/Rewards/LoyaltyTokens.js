0a314643779ffcca1bef2698714dc58f4aa1a009
Baibhav Kumar <baibhav.kumar.122003@gmail.com>
2023-08-16 19:36:50 +0530

    .

--- src/Components/Rewards/LoyaltyTokens.js
+++ src/Components/Rewards/LoyaltyTokens.js
@@ -1,11 +1,33 @@
-import React from 'react';
-import './LoyaltyTokens.css'; // Import the CSS file
+import React, { useEffect, useState } from "react";
+import "./LoyaltyTokens.css"; // Import the CSS file
 
 const UserLoyaltyTokens = () => {
-  // Replace with actual loyalty token data
-  const loyaltyTokens = [
-    { id: 1, amount: 100 },
-  ];
+  const [loyaltyTokens, setLoyaltyTokens] = useState([]);
+
+  const fetchResponse = async () => {
+    try {
+      const response = await fetch(
+        "http://localhost:8000/api/user/getTokenBalance",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        const responseData = await response.json();
+        setLoyaltyTokens(responseData); // Update the state with fetched data
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchResponse();
+  }, []);
 
   return (
     <div className="loyalty-tokens">
@@ -13,6 +35,6 @@ const UserLoyaltyTokens = () => {
       <p className="token-amount">{loyaltyTokens[0].amount} Tokens</p>
     </div>
   );
-}
+};
 
 export default UserLoyaltyTokens;
