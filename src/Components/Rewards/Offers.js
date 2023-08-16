4987a1cfb2854dd6febc6d39e4ec0dd834e77c73
Baibhav Kumar <baibhav.kumar.122003@gmail.com>
2023-08-16 19:14:03 +0530

    .

--- src/Components/Rewards/Offers.js
+++ src/Components/Rewards/Offers.js
@@ -4,17 +4,17 @@ import "./Offers.css";
 const Offers = () => {
   // Replace with actual offer data
 
-  useEffect(async () => {
-    const response = await fetch("http://localhost:8000/api/user/getOffers", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+  // useEffect(async () => {
+  //   const response = await fetch("http://localhost:8000/api/user/getOffers", {
+  //     method: "GET",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //   });
 
-    if (response.status === 200 && response) {
-    }
-  }, []);
+  //   if (response.status === 200 && response) {
+  //   }
+  // }, []);
 
   const offers = [
     { id: 1, name: "Discount on Product A", cost: 50 },
