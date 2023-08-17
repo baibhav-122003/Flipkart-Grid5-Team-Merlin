818a21e0d8f333b6427225ab9fc904993dd55727
Suman Sahoo <ssamsomu@gmail.com>
2023-08-17 13:05:04 +0530

    .

--- src/Components/Rewards/LoyaltyTokens.js
+++ src/Components/Rewards/LoyaltyTokens.js
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from "react";
 import "./LoyaltyTokens.css"; // Import the CSS file
 
 const UserLoyaltyTokens = () => {
-  const [loyaltyTokens, setLoyaltyTokens] = useState({ amount: 0 }); // Initialize with amount property
+  const [loyaltyTokens, setLoyaltyTokens] = useState({ amount: 0 });
+  const [earningRules, setEarningRules] = useState({});
 
   const fetchUserResponse = async (email) => {
-
     try {
       const response = await fetch(
         "http://localhost:8000/api/user/getTokenBalance",
@@ -32,7 +32,6 @@ const UserLoyaltyTokens = () => {
   };
 
   const fetchSellerResponse = async (email) => {
-
     console.log(email);
     try {
       const response = await fetch(
@@ -61,18 +60,45 @@ const UserLoyaltyTokens = () => {
     }
   };
 
+  const fetchEarningRules = async () => {
+    try {
+      const response = await fetch("http://localhost:8000/api/tokenrules");
+      if (response.status === 200) {
+        const data = await response.json();
+        setEarningRules(data);
+      } else {
+        console.error("Error fetching earning rules:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error fetching earning rules:", error);
+    }
+  };
+
   useEffect(() => {
     if (localStorage.getItem("userEmail")) {
       fetchUserResponse(localStorage.getItem("userEmail"));
     } else {
       fetchSellerResponse(localStorage.getItem("sellerEmail"));
     }
+    fetchEarningRules();
   }, []);
 
   return (
     <div className="loyalty-tokens">
-      <h2>Loyalty Tokens Earned</h2>
-      <p className="token-amount">{loyaltyTokens.amount} Tokens</p>
+      <div className="loyalty-tokens-content">
+        <div className="loyalty-tokens-section">
+          <h2>Loyalty Tokens Earned</h2>
+          <p className="token-amount">{loyaltyTokens.amount} Tokens</p>
+        </div>
+        <div className="earning-rules-section">
+          <div className="earning-rules">
+            <h3>Earning Rules</h3>
+            <p>Points per Penny: {earningRules.pointsPerPenny}</p>
+            <p>Points per Order: {earningRules.pointsPerOrder}</p>
+            <p>Tokens per Loyalty Point: {earningRules.tokensPerLoyaltyPoint}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
